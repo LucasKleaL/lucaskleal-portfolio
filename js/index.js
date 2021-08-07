f71bdeb8172f8f69086ccd610e61dcad44d2d3ce
LucasKleaL <lucaskleal222@outlook.com>
2021-08-07 16:11:00 -0300

    Portfolio 0.4
    -Adicionado botões e funções para a navegação entre as diferentes imagens dos projetos em some projects.
    -Alteração de atributos de algumas classes css para melhor adaptação responsiva à dispositivos móveis.

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ const languagesArray = [
     ["HTML", "I like html for the simple way <br> to the structure the project.", "80%", "html_wallpaper.jpg"],
     ["CSS", "For me css is the web piece where <br> I can release my design creativity.", "70%", "css_wallpaper.jpg"],
     ["Javascript", "Javascript is my favourite language on the moment, <br> is incredible his integration and possibilities.", "70%", "js_wallpaper.jpg"],
-    ["jQuery", "jQuery is the most useful and productive <br> framework for Javascript on my opinion.", "60%", "jquery_wallpaper.jpg"],
+    ["jQuery", "jQuery is the most useful and productive <br> Javascript library on my opinion.", "60%", "jquery_wallpaper.jpg"],
     ["ReactJS", "It is currently the technology that <br> I most want to develop my skills.", "20%", "react_wallpaper.jpg"],
     ["PHP", "Is the first backend language what I had <br> contact, is extremely simple to interact with SQL.", "40%", "php_wallpaper.jpg"],
     ["Python", "My first programming language, <br> where I learned logical thinking.", "40%", "python_wallpaper.jpg"],
@@ -25,11 +25,19 @@ const experiencePorcentage = [
 ];
 
 const projectsData = [
-    ["CLOSE!", "public/img/projects_wallpapers/close_home_wallpaper.png", "https://github.com/LucasKleaL/close", ["html", "css", "js", "jquery", "php", "sql"], "Completly platform for clothes/accesories e-commerce, integrated with postal send calculator, payment metods like paypal, inventory management and other functionalities."],
-    ["KMFLIX", "public/img/projects_wallpapers/kmflix_home_wallpaper.png", "https://github.com/VitorCRF/KMFlix", ["html", "css", "js", "jquery", "php", "sql"], "KMFLIX is a stream platform made for academic work. Based on Netflix platform, we have implemented the principals functionalities like signup, login, series/movies dashboard, title description, filters, reproduction page and other minor functionalities."],
-    ["Sleep Sounds", "public/img/projects_wallpapers/sleepsounds_home_wallpaper.png", "https://github.com/LucasKleaL/Sleep-Sounds", ["html", "css", "js", "jquery"], "Sleep Sounds is a white noise reproduction application based on PWA (Progressive Web Application) functionality."],
-    ["MoveIt", "public/img/projects_wallpapers/moveit_home_wallpaper.png", "https://github.com/LucasKleaL/MoveIt", ["css", "js", "reactjs", "typescript"], "MoveIt is a ReactJS application made for understanding of the basic concepts on React, like components and contexts. The basic idea behind of this app is time cycles on work. When the clock achieve 0 a simple challenge appear on the screen, if the user complete this, experience is obtained and the level up."],
-    ["Word Counter", "public/img/projects_wallpapers/wordcounter_home_wallpaper.png", "https://github.com/LucasKleaL/word-counter", ["html", "css", "js", "jquery"], "This is a simple website with a word counter."]
+    ["CLOSE!", "0", "https://github.com/LucasKleaL/close", ["html", "css", "js", "jquery", "php", "sql"], "Completly platform for clothes/accesories e-commerce, integrated with postal send calculator, payment metods like paypal, inventory management and other functionalities."],
+    ["KMFLIX", "1", "https://github.com/VitorCRF/KMFlix", ["html", "css", "js", "jquery", "php", "sql"], "KMFLIX is a stream platform made for academic work. Based on Netflix platform, we have implemented the principals functionalities like signup, login, series/movies dashboard, title description, filters, reproduction page and other minor functionalities."],
+    ["Sleep Sounds", "2", "https://github.com/LucasKleaL/Sleep-Sounds", ["html", "css", "js", "jquery"], "Sleep Sounds is a white noise reproduction application based on PWA (Progressive Web Application) functionality."],
+    ["MoveIt", "3", "https://github.com/LucasKleaL/MoveIt", ["css", "js", "reactjs", "typescript"], "MoveIt is a ReactJS application made for understanding of the basic concepts on React, like components and contexts. The basic idea behind of this app is time cycles on work. When the clock achieve 0 a simple challenge appear on the screen, if the user complete this, experience is obtained and the level up."],
+    ["Word Counter", "4", "https://github.com/LucasKleaL/word-counter", ["html", "css", "js", "jquery"], "This is a simple website with a word counter."]
+];
+
+const projectsImgData = [
+    ["close_home_wallpaper.png"],
+    ["kmflix_home_wallpaper.png", "kmflix_login_wallpaper.png"],
+    ["sleepsounds_home_wallpaper.png"],
+    ["moveit_home_wallpaper.png"],
+    ["wordcounter_home_wallpaper.png"]
 ];
 
 $(document).ready(function() {
@@ -73,7 +81,11 @@ $(document).ready(function() {
 
         for (var i = 0; i < projectsData.length; i++) {
             content += '<div class="div-container-project">';
-            content += '<img class="img-project" src="'+projectsData[i][1]+'">';
+            content += '<div class="div-project-img">';
+            content += '<img class="arrow-icon" id="buttonBack'+projectsData[i][0]+'" value="'+projectsData[i][1]+'" onclick="backProjectImg(this.id)" style="float: left;" src="public/icons/left_arrow_icon.png">';
+            content += '<img class="img-project" id="imgProject'+projectsData[i][1]+'" value="0" src="public/img/projects_wallpapers/'+projectsImgData[i][0]+'">';
+            content += '<img class="arrow-icon" id="buttonNext'+projectsData[i][0]+'" value="'+projectsData[i][1]+'" onclick="nextProjectImg(this.id)" style="float: right;" src="public/icons/right_arrow_icon.png">';
+            content += '</div>';
             content += '<div class="div-project-attributes">';
             content += '<h1 class="h1-project-title">'+projectsData[i][0]+'</h1>';
             content += '<p class="p-project-desc">'+projectsData[i][4]+'</p>';
@@ -125,8 +137,28 @@ $(document).ready(function() {
 
         }
 
-        
-
     }
-        
+
 });
+
+function nextProjectImg(id) {
+
+    var getValue = document.getElementById(id).getAttribute("value");
+    var getImgValue = parseInt(document.getElementById("imgProject"+getValue).getAttribute("value"));
+    var index = getImgValue + 1;
+
+    $("#imgProject"+getValue).attr("src", "public/img/projects_wallpapers/"+projectsImgData[getValue][index]);
+    $("#imgProject"+getValue).attr("value", getImgValue+1);
+
+}
+
+function backProjectImg(id) {
+
+    var getValue = document.getElementById(id).getAttribute("value");
+    var getImgValue = parseInt(document.getElementById("imgProject"+getValue).getAttribute("value"));
+    var index = getImgValue - 1;
+
+    $("#imgProject"+getValue).attr("src", "public/img/projects_wallpapers/"+projectsImgData[getValue][index]);
+    $("#imgProject"+getValue).attr("value", getImgValue-1);
+
+}
